9385203fb067a4babf742bfe6fbd3d40350607af
tom <tom-kccheng@gmail.com>
2025-12-28 02:33:04 -0500

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState, useEffect, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [isTalking, setIsTalking] = useState(false);
-  const [logs, setLogs] = useState(["Hello! It is your voice assistant. I am here to help you make a new schefule on Google Calendar. Let me know how can I help!"]);
+  const [logs, setLogs] = useState(["Hello! It is your voice assistant. I am here to help you make a new schedule on Google Calendar. Let me know how can I help!"]);
 
-  const logEndRef = useRef(null); // 1. Create the ref
+  const logEndRef = useRef<HTMLDivElement>(null);
 
-  // 2. This hook watches the logs. Every time they change, it scrolls.
+  // Every time sth added to logs, it scrolls.
   useEffect(() => {
     logEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [logs]);
@@ -21,13 +18,12 @@ function App() {
 
     const timestamp = new Date().toLocaleTimeString();
     const message = newState ? "Started talking..." : "Stopped talking.";
-    // setLogs((prev) => [`[${timestamp}] ${message}`, ...prev]);
     setLogs((prev) => [...prev, `[${timestamp}] ${message}`]);
   };
 
   return (
     <div className="p-8 font-sans">
-      {/* Button with dynamic Tailwind classes */}
+      {/* Button to talk/stop */}
       <button
         onClick={toggleTalk}
         className={`px-6 py-2 rounded-lg font-semibold text-white transition-colors duration-200 
@@ -35,19 +31,20 @@ function App() {
       >
         {isTalking ? 'Stop' : 'Talk'}
       </button>
+      
+      {/* padding */}
+      <div className="h-[10px]" />
 
-
-      <div className="w-[300px] h-[300px] border-4 border-[#121212] box-border overflow-y-auto text-left">
-
+      {/* ui for the log board */}
+      <div className="w-[600px] h-[300px] border-4 border-[#121212] box-border overflow-y-auto text-left">
         <div className="space-y-2">
-            {logs.map((log, i) => (
-              <div key={i} className="animate-in fade-in slide-in-from-left-2 duration-300">
-                {log}
-              </div>
-            ))}
-            <div ref={logEndRef} />
-          </div>
-          
+          {logs.map((log, i) => (
+            <div key={i} className="animate-in fade-in slide-in-from-left-2 duration-300">
+              {log}
+            </div>
+          ))}
+          <div ref={logEndRef} />
+        </div>
       </div>
 
     </div>
